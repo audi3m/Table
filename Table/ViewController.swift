818d244d8aa5e706eca812d5022e8d7fe9984dd2
ray <audi3m@naver.com>
2024-05-23 16:33:55 +0900

    Initial Commit

---
+++ Table/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Table
+//
+//  Created by J Oh on 5/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
